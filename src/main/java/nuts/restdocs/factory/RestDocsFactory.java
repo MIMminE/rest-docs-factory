0edc7feb814e670d37440cb87fdb7150b5cec889
MIMminE <weluv.ww@gmail.com>
2024-04-16 12:40:40 +0900

    그레들 수정

--- src/main/java/nuts/restdocs/factory/RestDocsFactory.java
+++ src/main/java/nuts/restdocs/factory/RestDocsFactory.java
@@ -6,6 +6,7 @@ import org.springframework.restdocs.payload.ResponseFieldsSnippet;
 
 public interface RestDocsFactory {
 
+
     RestDocumentationResultHandler document(String identifier, Class<?> requestClass, Class<?> responseClass);
     RequestFieldsSnippet requestFields(Class<?> requestClass);
     ResponseFieldsSnippet responseFields(Class<?> responesClass);
